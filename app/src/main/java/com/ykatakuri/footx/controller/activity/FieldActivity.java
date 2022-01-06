2d692eff52c85fde64f46880e2c7b8182ac83b85
ykatakuri <yannatkk@gmail.com>
2022-01-06 18:11:18 +0100

    navigation to field activity

---
+++ app/src/main/java/com/ykatakuri/footx/controller/activity/FieldActivity.java
@@ -0,0 +1,16 @@
+package com.ykatakuri.footx.controller.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ykatakuri.footx.R;
+
+public class FieldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_field);
+    }
+}
