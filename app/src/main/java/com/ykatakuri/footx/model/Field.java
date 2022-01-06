3918e9391376939ce9ccd073e54ccf33bf935849
ykatakuri <yannatkk@gmail.com>
2022-01-06 18:54:18 +0100

    field model created

--- app/src/main/java/com/ykatakuri/footx/model/Field.java
+++ app/src/main/java/com/ykatakuri/footx/model/Field.java
@@ -1,2 +1,52 @@
-package com.ykatakuri.footx.model;public class Field {
+package com.ykatakuri.footx.model;
+
+public class Field {
+    private String imageName;
+    private String fieldName;
+    private String description;
+    private String address;
+
+    public Field(String imageName, String fieldName, String description, String address) {
+        this.imageName= imageName;
+        this.fieldName= fieldName;
+        this.description= description;
+        this.address = address;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString()  {
+        return this.fieldName+" (Adresse: "+ this.address+")";
+    }
 }
