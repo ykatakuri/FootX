54f46aa6760761a6ec9bcb8eac9b307c6def84ba
ykatakuri <yannatkk@gmail.com>
2022-01-06 15:53:44 +0100

    fragments adapter added

--- app/src/main/java/com/ykatakuri/footx/controller/TabFragmentAdapter.java
+++ app/src/main/java/com/ykatakuri/footx/controller/TabFragmentAdapter.java
@@ -1,2 +1,35 @@
-package com.ykatakuri.footx.controller;public class TabFragmentAdapter {
+package com.ykatakuri.footx.controller;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.ykatakuri.footx.controller.fragment.EventFragment;
+import com.ykatakuri.footx.controller.fragment.SuggestionFragment;
+
+public class TabFragmentAdapter extends FragmentStateAdapter {
+
+    public TabFragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position)
+        {
+            case 1 :
+                return new SuggestionFragment();
+        }
+
+        return new EventFragment();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
 }
