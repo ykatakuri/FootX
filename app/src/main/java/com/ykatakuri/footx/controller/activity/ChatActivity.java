9577356403bc8a9008e323bee9b363297475f882
ykatakuri <yannatkk@gmail.com>
2022-01-21 17:05:00 +0100

    model fro chat messages created

--- app/src/main/java/com/ykatakuri/footx/controller/activity/ChatActivity.java
+++ app/src/main/java/com/ykatakuri/footx/controller/activity/ChatActivity.java
@@ -2,8 +2,12 @@ package com.ykatakuri.footx.controller.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 import com.ykatakuri.footx.R;
 
 public class ChatActivity extends AppCompatActivity {
@@ -12,5 +16,24 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+
+        } else {
+            Toast.makeText(this,
+                    "Welcome " + FirebaseAuth.getInstance()
+                            .getCurrentUser()
+                            .getDisplayName(),
+                    Toast.LENGTH_LONG)
+                    .show();
+
+            displayChatMessages();
+        }
+    }
+
+    private void displayChatMessages() {
+
     }
 }
