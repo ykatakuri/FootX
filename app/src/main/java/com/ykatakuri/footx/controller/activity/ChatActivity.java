3a326235b264859d21010441379ee3c7c001b61f
ykatakuri <yannatkk@gmail.com>
2022-01-21 17:11:19 +0100

    post message done

--- app/src/main/java/com/ykatakuri/footx/controller/activity/ChatActivity.java
+++ app/src/main/java/com/ykatakuri/footx/controller/activity/ChatActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 import com.ykatakuri.footx.R;
+import com.ykatakuri.footx.model.ChatMessage;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private FloatingActionButton mSendMessageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,25 @@ public class ChatActivity extends AppCompatActivity {
 
             displayChatMessages();
         }
+
+        mSendMessageButton = findViewById(R.id.activity_chat_fab);
+
+        mSendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText input = (EditText)findViewById(R.id.activity_chat_input);
+
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new ChatMessage(input.getText().toString(),
+                                FirebaseAuth.getInstance()
+                                        .getCurrentUser()
+                                        .getDisplayName())
+                        );
+                input.setText("");
+            }
+        });
     }
 
     private void displayChatMessages() {
