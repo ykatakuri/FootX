25c89a03f362607e78f13bec4631b9f7c26cd2b0
ykatakuri <yannatkk@gmail.com>
2022-01-21 15:22:41 +0100

    controllers for suggestion screen done

---
+++ app/src/main/java/com/ykatakuri/footx/adapter/SuggestionRecyclerViewAdapter.java
@@ -0,0 +1,84 @@
+package com.ykatakuri.footx.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ykatakuri.footx.R;
+import com.ykatakuri.footx.model.Events;
+
+import java.util.ArrayList;
+
+public class SuggestionRecyclerViewAdapter extends RecyclerView.Adapter<SuggestionRecyclerViewAdapter.ViewHolder> {
+    private ArrayList<Events> eventsArrayList;
+    private Context context;
+    private EventClickInterface eventClickInterface;
+    int lastPos = -1;
+
+    public SuggestionRecyclerViewAdapter(ArrayList<Events> eventsArrayList, Context context, EventClickInterface eventClickInterface) {
+        this.eventsArrayList = eventsArrayList;
+        this.context = context;
+        this.eventClickInterface = eventClickInterface;
+    }
+
+    @NonNull
+    @Override
+    public SuggestionRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.event_recyclerview_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SuggestionRecyclerViewAdapter.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        Events event = eventsArrayList.get(position);
+        holder.mFieldNameTextView.setText(event.getField());
+        holder.mDateTextView.setText(event.getDate() +" à "+event.getTime());
+
+        setAnimation(holder.itemView, position);
+        holder.mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eventClickInterface.onEventClick(position);
+            }
+        });
+    }
+
+    private void setAnimation(View itemView, int position) {
+        if (position > lastPos) {
+            Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
+            itemView.setAnimation(animation);
+            lastPos = position;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventsArrayList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mFieldNameTextView, mAuthorTextView, mDateTextView, mTimeTextView, mFormatTextView;
+        private ImageView mImageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.event_recyclerview_imageview);
+            mFieldNameTextView = itemView.findViewById(R.id.event_recyclerview_fieldname);
+            mDateTextView = itemView.findViewById(R.id.event_recyclerview_date_time);
+        }
+    }
+
+    public interface EventClickInterface {
+        void onEventClick(int position);
+    }
+}
--- app/src/main/java/com/ykatakuri/footx/controller/fragment/SuggestionFragment.java
+++ app/src/main/java/com/ykatakuri/footx/controller/fragment/SuggestionFragment.java
@@ -1,61 +1,141 @@
 package com.ykatakuri.footx.controller.fragment;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.firebase.ui.database.FirebaseRecyclerOptions;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.ykatakuri.footx.R;
-import com.ykatakuri.footx.adapter.SuggestionAdapter;
+import com.ykatakuri.footx.adapter.SuggestionRecyclerViewAdapter;
+import com.ykatakuri.footx.controller.activity.EditActivity;
+import com.ykatakuri.footx.controller.activity.MainActivity;
 import com.ykatakuri.footx.model.Events;
+
+import java.util.ArrayList;
 
 public class SuggestionFragment extends Fragment {
 
-    Activity mContext;
-
-    private RecyclerView recyclerView;
-    SuggestionAdapter adapter;
+    FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+    private RecyclerView mRecyclerView;
+    private FirebaseAuth firebaseAuth;
+    private ProgressBar mProgressBar;
+    private ArrayList<Events> mEventsArrayList;
+    private SuggestionRecyclerViewAdapter suggestionRecyclerViewAdapter;
+    private RelativeLayout mSuggestionRelativeLayoutMain;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        mContext = getActivity();
-
         View view = inflater.inflate(R.layout.fragment_suggestion, container, false);
 
-        databaseReference = FirebaseDatabase.getInstance().getReference("Events");
-        recyclerView = view.findViewById(R.id.suggestion_recyclerview);
+        mRecyclerView = view.findViewById(R.id.suggestion_recyclerview);
+        mSuggestionRelativeLayoutMain = view.findViewById(R.id.suggestion_relative_layout_main);
+        mProgressBar = view.findViewById(R.id.suggestion_progressbar);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        mEventsArrayList = new ArrayList<>();
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        databaseReference = firebaseDatabase.getReference("Events");
 
-        FirebaseRecyclerOptions<Events> options = new FirebaseRecyclerOptions.Builder<Events>().setQuery(databaseReference, Events.class).build();
-        adapter = new SuggestionAdapter(options);
-        recyclerView.setAdapter(adapter);
+        suggestionRecyclerViewAdapter = new SuggestionRecyclerViewAdapter(mEventsArrayList, getContext(), this::onEventClick);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.setAdapter(suggestionRecyclerViewAdapter);
+
+        getEvents();
 
         return view;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        adapter.startListening();
+    private void getEvents() {
+        mEventsArrayList.clear();
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                mProgressBar.setVisibility(View.GONE);
+                mEventsArrayList.add(snapshot.getValue(Events.class));
+                suggestionRecyclerViewAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                mProgressBar.setVisibility(View.GONE);
+                suggestionRecyclerViewAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                suggestionRecyclerViewAdapter.notifyDataSetChanged();
+                mProgressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                suggestionRecyclerViewAdapter.notifyDataSetChanged();
+                mProgressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        adapter.stopListening();
+    private void onEventClick(int position) {
+        displayBottomSheet(mEventsArrayList.get(position));
+    }
+
+    private void displayBottomSheet(Events event) {
+        final BottomSheetDialog bottomSheetTeachersDialog = new BottomSheetDialog(this.getContext(), R.style.BottomSheetDialogTheme);
+        View layout = LayoutInflater.from(this.getContext()).inflate(R.layout.bottom_sheet_layout_suggestion, mSuggestionRelativeLayoutMain, false);
+        bottomSheetTeachersDialog.setContentView(layout);
+        bottomSheetTeachersDialog.setCancelable(false);
+        bottomSheetTeachersDialog.setCanceledOnTouchOutside(true);
+        bottomSheetTeachersDialog.show();
+
+        TextView field = layout.findViewById(R.id.bottom_sheet_suggestion_field_name);
+        TextView date = layout.findViewById(R.id.bottom_sheet_suggestion_date);
+        TextView time = layout.findViewById(R.id.bottom_sheet_suggestion_time);
+        TextView format = layout.findViewById(R.id.bottom_sheet_suggestion_format);
+        ImageView imageView = layout.findViewById(R.id.bottom_sheet_suggestion_imageview);
+
+        field.setText(event.getField());
+        date.setText(event.getDate());
+        time.setText(event.getTime());
+        format.setText(event.getFormat());
+
+        Button participateButton = layout.findViewById(R.id.bottom_sheet_suggestion_button_participate);
+
+        participateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Participation acceptée...", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
